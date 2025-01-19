49bd7792f175d7d76a88d769a617df23c8c93c79
luzdobby <luzmariamontano274@gmai.com>
2025-01-19 00:32:39 -0600

    JS

---
+++ JS/index.js
@@ -0,0 +1,21 @@
+let boton=document.getElementById("btnInciar");
+
+
+boton=addEventListener('click',()=>{
+
+if(window.location.href='ALMACEN_INSUMOS/almacen.html'){
+
+
+alert("Bienvenido");
+
+
+}
+else{
+
+alert("URL NO ENCONTRADA");
+
+}
+
+
+
+});
